80896aceeaf2069157355e6993f6f92010ce889a
Deepthi <11665833+deeptijnair@users.noreply.github.com>
2025-08-05 10:40:29 -0400

    Added a clear button to clear the list of all items

--- list.js
+++ list.js
@@ -1,5 +1,20 @@
-function addGroceryItem() {
+let items = [];
+
+const addButton = document.getElementById("add-btn");
+const clearButton = document.getElementById("clear-btn");
+
+addButton.addEventListener("click", () => {
   let groceryInput = document.getElementById("grocery-input").value;
   console.log("The grocery input is: ", groceryInput);
-  document.getElementById("grocery-list").innerHTML += `<li>${groceryInput}</li>`;
-}
+  document.getElementById(
+    "grocery-list"
+  ).innerHTML += `<li>${groceryInput}</li>`;
+  items.push(groceryInput);
+});
+
+
+clearButton.addEventListener("click", () => {
+  document.getElementById("grocery-list").innerHTML = "";
+  items = [];
+  console.log("The items array: ", items)
+});
